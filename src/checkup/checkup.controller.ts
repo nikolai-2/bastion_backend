b7d9ac949fe56d27cd4a7f6bdd3b1b402ad9041c
CptIdea <pliyznik@yandex.ru>
2021-03-20 02:24:09 +0300

    Checkup controller

--- src/checkup/checkup.controller.ts
+++ src/checkup/checkup.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards,Request } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CheckupService } from './checkup.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('checkup')
 export class CheckupController {
+  constructor(private checkupService:CheckupService){}
+
   @Get('{date}/getList')
-  async getList(@Param('date') date: string) {}
+  async getList(@Request() req,@Param('date') date: string) {
+    var d = new Date(date)
+    d.setHours(0,0,0,0) 
+    return await this.checkupService.getList(req.user,d)
+  }
 }
