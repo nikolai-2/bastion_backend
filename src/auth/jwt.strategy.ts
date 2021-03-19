e243e8097badef5946e16719d0037edcb9a4c6e1
Efim Belyakov <belyakov.efim@gmail.com>
2021-03-20 01:58:28 +0300

    Получение юзера из jwt токена

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: any) {
     return await this.userService.user({
-      id: payload.sub
+      id: payload.sub,
     });
   }
 }
