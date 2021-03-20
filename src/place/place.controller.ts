7615ef3e30ed75f267be6a580a9e0283ca1ce40f
CptIdea <pliyznik@yandex.ru>
2021-03-20 16:37:01 +0300

    Roles separate for place

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,19 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PlaceService } from './place.service';
 import { PlaceInputDto } from './place-input.dto';
+import { RolesGuard } from 'src/role/roles.guard';
+import { Roles } from 'src/role/roles.decorator';
+import { Role } from 'src/role/roles.enum';
 
 @Controller('place')
 @ApiTags('place')
+@UseGuards(RolesGuard)
 export class PlaceController {
   constructor(private placeService: PlaceService) {}
 
+  @Roles(Role.Guard,Role.Boss)
   @Get('get')
   @ApiOperation({ summary: 'Возвращает объект охраны' })
   getPlace() {
     return this.placeService.getPlace();
   }
 
+  @Roles(Role.Boss)
   @Post('create')
   @ApiOperation({ summary: 'Создает новый объект охраны' })
   async createPlace(@Body() placeInputDto: PlaceInputDto) {
@@ -25,6 +31,7 @@ export class PlaceController {
     });
   }
 
+  @Roles(Role.Boss)
   @Get('remove')
   @ApiOperation({ summary: 'Удаляет объект охраны' })
   removePlace() {
