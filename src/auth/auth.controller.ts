ddfc39fc87ad10dd06fbcf8a9de2629f4b4f1e43
Efim Belyakov <belyakov.efim@gmail.com>
2021-03-20 15:38:27 +0300

    Возвращение информации о пользователе по токену

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserLoginDto } from './user-login.dto';
 import { LoginResponseInterface } from './login-response.interface';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -19,5 +21,17 @@ export class AuthController {
   @ApiBody({ type: UserLoginDto })
   async login(@Request() req): Promise<LoginResponseInterface> {
     return this.authService.getToken(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('getUser')
+  async getUser(@Request() req) {
+    const user: User = req.user;
+    const { name, avatar_src, role, ...o } = user;
+    return {
+      name: name,
+      avatar_src: avatar_src,
+      role: role,
+    };
   }
 }
